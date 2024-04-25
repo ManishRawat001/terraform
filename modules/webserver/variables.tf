040c3811c5ce94b54e9434192a2156e55993a21a
ManishRawat001 <manishtoawsl2@gmail.com>
2024-04-25 15:55:42 +0300

    modularize tf project

---
+++ modules/webserver/variables.tf
@@ -0,0 +1,11 @@
+variable vpc_id {}
+variable my_ip {}
+variable env_prefix {}
+variable image_name {}
+variable public_key_path {}
+variable instance_type {}
+variable subnet_id {}
+variable avail_zone {}
+
+
+
