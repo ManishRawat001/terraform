c749a1edb3c90e5c9a25ea26d843027f1ba352d0
ManishRawat001 <manishtoawsl2@gmail.com>
2024-04-25 13:47:26 +0300

    provisioner related commits

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ variable env_prefix {}
 variable my_ip {}
 variable instance_type {}
 variable public_key_path {}
+variable private_key_path {}
 
 resource "aws_vpc" "myapp-vpc" {
     cidr_block = var.vpc_cidr_block
@@ -104,7 +105,34 @@ resource "aws_instance" "myapp-server" {
     associate_public_ip_address = true
     key_name = aws_key_pair.ssh-key.key_name
 
-    user_data = file("entry-script.sh")
+    # user_data = file("entry-script.sh")
+    connection {
+        type = "ssh"
+        host = self.public_ip
+        user = "ec2-user"
+        private_key = file(var.private_key_path)
+        timeout  = "300s"
+    }
+
+    #provisioner 1: this provisioner is used to copy files or directories from local to newly created resources
+    provisioner "file" { 
+        source = "entry-script.sh"
+        destination = "/home/ec2-user/entry-script-on-ec2.sh"
+    }
+
+    #provisioner 2:
+    provisioner "remote-exec" {
+        inline = [
+            "mkdir manish"
+        ]
+        # script = file("entry-script-on-ec2.sh")
+    }
+
+    #provisioner 3: this provisioner invokes a local executable after a resource is created
+    provisioner "local-exec" {
+        command = "echo ${self.public_ip} > output.txt"
+    }
+
     tags = {
         Name = "${var.env_prefix}-server"
     }
